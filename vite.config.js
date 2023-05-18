f933fb55040f233eb7781fbf909fff0e7ad9a02b
AmaruVL <182940@unsaac.edu.pe>
2023-05-18 09:40:34 -0500

    Puerto añadido desde .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const PORT = parseInt(process.env.VITE_PORT) || 5171;
+
+  return defineConfig({
+    plugins: [react()],
+
+    server: {
+      port: PORT,
+    },
+    preview: {
+      port: PORT,
+    },
+  });
+};
